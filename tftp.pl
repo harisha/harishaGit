9e52bb33d1ec5009473a3c93a56b130151b8aec1
Harisha <haricubbon@gmail.com>
2011-07-03 08:57:18 +0530

    Modified for Command Line Arguments

--- tftp.pl
+++ tftp.pl
@@ -2,11 +2,37 @@
 
 use warnings;
 use strict;
-
 use TFTP;
 
+my $numOfArgs=$#ARGV+1;
+
+if($numOfArgs != 3)
+{
+	print "\n Usage: tftp.pl <IpAddress> <cmd-get/put> <fileName>";
+	exit;
+}
+
 my $tftp;
-$tftp=new TFTP("192.168.1.2") or die "TFTP object failed $@";
-$tftp->get("Hello.txt")  or die "TFTP get failed $@";
+
+$tftp=new TFTP($ARGV[0]) or die "object creation failed $@";
+if($ARGV[1] eq "get")
+{
+	print "File --> $ARGV[2]\n";
+	$tftp->get($ARGV[2])  or die "get fail $@";
+}
+elsif($ARGV[1] eq "put")
+{
+	print "File --> $ARGV[2]\n";
+	$tftp->octet;
+	$tftp->put($ARGV[2])  or die "put fail $!";
+}
+else
+{
+	print "Please Enter Valid TFTP Command \n";
+}
+
 $tftp->quit or die "TFTP quit failed $@";
+
+
+
 
